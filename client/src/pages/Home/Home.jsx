76d4144156783ffe08214509cfa9745f44323800
Zeyad Farhat <zeyadfarhat145@gmail.com>
2023-07-21 17:29:17 +0300

    upload

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,20 +1,35 @@
+import api from "../../utils/api";
 import "./home.scss";
-
+import { useEffect, useState } from "react";
 function Home() {
+  const [data, setData] = useState();
+  const loadDate = async () => {
+    try {
+      const response = await api.get("/home");
+      console.log(response);
+      setData(response.data);
+    } catch (err) {
+      alert("حدث خطأ ما");
+      console.log(err);
+    }
+  };
+  useEffect(() => {
+    loadDate();
+  }, []);
   return (
     <main id="home">
       <div className="container box-grid">
         <div className="box">
           <h3 className="label">عدد العملاء</h3>
-          <h3 className="value">50</h3>
+          <h3 className="value">{data?.clientsCount}</h3>
         </div>
         <div className="box">
           <h3 className="label">عدد الفواتير</h3>
-          <h3 className="value">50</h3>
+          <h3 className="value">{data?.invoicesCount}</h3>
         </div>
         <div className="box">
           <h3 className="label">باقي المستحقات</h3>
-          <h3 className="value">150,000</h3>
+          <h3 className="value">{data?.remainingDebt}</h3>
         </div>
       </div>
     </main>
